e4accfb9a3ae44dad4cc94eb665adcc3d18cf42b
Daryl Hornick-Becker <darylhornickbecker@gmail.com>
2025-05-28 18:30:55 +0200

    Just need help on post route

--- api/folders.js
+++ api/folders.js
@@ -1,3 +1,4 @@
+import { createFile } from "#db/queries/files"
 import { getFolders, getFoldersByIdIncludingFiles } from "#db/queries/folders"
 import express from "express"
 const foldersRouter = express.Router()
@@ -18,4 +19,29 @@ foldersRouter.route("/:id").get(async(req, res)=>{
         return res.status(404).send("There is no folder with that id")
     }
     res.send(foundFolder)
+})
+
+//POST /folders/:id/files
+foldersRouter.route("/:id/files").post(async(req, res)=>{
+
+const id = Number(req.params.id)
+const foundFolder = await getFoldersByIdIncludingFiles(id)
+
+    if (!foundFolder){
+        return res.status(404).send("There is no folder with that id")
+    }
+
+if (!req.body){
+    return res.status(400).send("Missing request body")
+}
+
+const {name, size, folder_id} = req.body
+
+if(!name || !size || !folder_id){
+    return res.status(400).send("Missing required fields")
+}
+
+const newFile = await createFile({name, size, folder_id})
+res.status(201).send(newFile)
+
 })
